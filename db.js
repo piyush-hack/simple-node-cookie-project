7d38ac29d8f5bae67ac52245bb871cb2b161d727
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-07-25 12:41:14 +0530

    new ch

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/mydb";
+const mongoURI = process.env.MONGO_URI || "mongodb+srv://Piyush:piyush2001@cluster0.iltke.mongodb.net/test";
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
